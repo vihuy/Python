14f4eb2ec03817c7a626bdabed0e743bb7d12f42
= <=>
2017-08-30 11:34:45 -0700

    Java Developer

---
+++ Chamcong.java
@@ -0,0 +1,548 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DoiTuong;
+
+/**
+ *
+ * @author Hieu Nguyen
+ */
+public class Chamcong {
+    private int Thang;
+    private String Manhanvien;
+    private String Ngay1;
+    private String Ngay2;
+    private String Ngay3;
+    private String Ngay4;
+    private String Ngay5;
+    private String Ngay6;
+    private String Ngay7;
+    private String Ngay8;
+    private String Ngay9;
+    private String Ngay10;
+    private String Ngay11;
+    private String Ngay12;
+    private String Ngay13;
+    private String Ngay14;
+    private String Ngay15;
+    private String Ngay16;
+    private String Ngay17;
+    private String Ngay18;
+    private String Ngay19;
+    private String Ngay20;
+    private String Ngay21;
+    private String Ngay22;
+    private String Ngay23;
+    private String Ngay24;
+    private String Ngay25;
+    private String Ngay26;
+    private String Ngay27;
+    private String Ngay28;
+    private String Ngay29;
+    private String Ngay30;
+    private String Ngay31;
+
+    public Chamcong(int Thang,String Manhanvien)
+    {
+        this.Thang=Thang;
+        this.Manhanvien=Manhanvien;
+        this.Ngay1="000";
+        this.Ngay2="000";
+        this.Ngay3="000";
+        this.Ngay4="000";
+        this.Ngay5="000";
+        this.Ngay6="000";
+        this.Ngay7="000";
+        this.Ngay8="000";
+        this.Ngay9="000";
+        this.Ngay10="000";
+        this.Ngay11="000";
+        this.Ngay12="000";
+        this.Ngay13="000";
+        this.Ngay14="000";
+        this.Ngay15="000";
+        this.Ngay16="000";
+        this.Ngay17="000";
+        this.Ngay18="000";
+        this.Ngay19="000";
+        this.Ngay20="000";
+        this.Ngay21="000";
+        this.Ngay22="000";
+        this.Ngay23="000";
+        this.Ngay24="000";
+        this.Ngay25="000";
+        this.Ngay26="000";
+        this.Ngay27="000";
+        this.Ngay28="000";
+        this.Ngay29="000";
+        this.Ngay30="000";
+        this.Ngay31="000";
+    }
+    public Chamcong()
+    {
+        
+    }
+    /**
+     * @return the Thang
+     */
+    public int getThang() {
+        return Thang;
+    }
+
+    /**
+     * @param Thang the Thang to set
+     */
+    public void setThang(int Thang) {
+        this.Thang = Thang;
+    }
+
+    /**
+     * @return the Manhanvien
+     */
+    public String getManhanvien() {
+        return Manhanvien;
+    }
+
+    /**
+     * @param Manhanvien the Manhanvien to set
+     */
+    public void setManhanvien(String Manhanvien) {
+        this.Manhanvien = Manhanvien;
+    }
+
+    /**
+     * @return the Ngay1
+     */
+    public String getNgay1() {
+        return Ngay1;
+    }
+
+    /**
+     * @param Ngay1 the Ngay1 to set
+     */
+    public void setNgay1(String Ngay1) {
+        this.Ngay1 = Ngay1;
+    }
+
+    /**
+     * @return the Ngay2
+     */
+    public String getNgay2() {
+        return Ngay2;
+    }
+
+    /**
+     * @param Ngay2 the Ngay2 to set
+     */
+    public void setNgay2(String Ngay2) {
+        this.Ngay2 = Ngay2;
+    }
+
+    /**
+     * @return the Ngay3
+     */
+    public String getNgay3() {
+        return Ngay3;
+    }
+
+    /**
+     * @param Ngay3 the Ngay3 to set
+     */
+    public void setNgay3(String Ngay3) {
+        this.Ngay3 = Ngay3;
+    }
+
+    /**
+     * @return the Ngay4
+     */
+    public String getNgay4() {
+        return Ngay4;
+    }
+
+    /**
+     * @param Ngay4 the Ngay4 to set
+     */
+    public void setNgay4(String Ngay4) {
+        this.Ngay4 = Ngay4;
+    }
+
+    /**
+     * @return the Ngay5
+     */
+    public String getNgay5() {
+        return Ngay5;
+    }
+
+    /**
+     * @param Ngay5 the Ngay5 to set
+     */
+    public void setNgay5(String Ngay5) {
+        this.Ngay5 = Ngay5;
+    }
+
+    /**
+     * @return the Ngay6
+     */
+    public String getNgay6() {
+        return Ngay6;
+    }
+
+    /**
+     * @param Ngay6 the Ngay6 to set
+     */
+    public void setNgay6(String Ngay6) {
+        this.Ngay6 = Ngay6;
+    }
+
+    /**
+     * @return the Ngay7
+     */
+    public String getNgay7() {
+        return Ngay7;
+    }
+
+    /**
+     * @param Ngay7 the Ngay7 to set
+     */
+    public void setNgay7(String Ngay7) {
+        this.Ngay7 = Ngay7;
+    }
+
+    /**
+     * @return the Ngay8
+     */
+    public String getNgay8() {
+        return Ngay8;
+    }
+
+    /**
+     * @param Ngay8 the Ngay8 to set
+     */
+    public void setNgay8(String Ngay8) {
+        this.Ngay8 = Ngay8;
+    }
+
+    /**
+     * @return the Ngay9
+     */
+    public String getNgay9() {
+        return Ngay9;
+    }
+
+    /**
+     * @param Ngay9 the Ngay9 to set
+     */
+    public void setNgay9(String Ngay9) {
+        this.Ngay9 = Ngay9;
+    }
+
+    /**
+     * @return the Ngay10
+     */
+    public String getNgay10() {
+        return Ngay10;
+    }
+
+    /**
+     * @param Ngay10 the Ngay10 to set
+     */
+    public void setNgay10(String Ngay10) {
+        this.Ngay10 = Ngay10;
+    }
+
+    /**
+     * @return the Ngay11
+     */
+    public String getNgay11() {
+        return Ngay11;
+    }
+
+    /**
+     * @param Ngay11 the Ngay11 to set
+     */
+    public void setNgay11(String Ngay11) {
+        this.Ngay11 = Ngay11;
+    }
+
+    /**
+     * @return the Ngay12
+     */
+    public String getNgay12() {
+        return Ngay12;
+    }
+
+    /**
+     * @param Ngay12 the Ngay12 to set
+     */
+    public void setNgay12(String Ngay12) {
+        this.Ngay12 = Ngay12;
+    }
+
+    /**
+     * @return the Ngay13
+     */
+    public String getNgay13() {
+        return Ngay13;
+    }
+
+    /**
+     * @param Ngay13 the Ngay13 to set
+     */
+    public void setNgay13(String Ngay13) {
+        this.Ngay13 = Ngay13;
+    }
+
+    /**
+     * @return the Ngay14
+     */
+    public String getNgay14() {
+        return Ngay14;
+    }
+
+    /**
+     * @param Ngay14 the Ngay14 to set
+     */
+    public void setNgay14(String Ngay14) {
+        this.Ngay14 = Ngay14;
+    }
+
+    /**
+     * @return the Ngay15
+     */
+    public String getNgay15() {
+        return Ngay15;
+    }
+
+    /**
+     * @param Ngay15 the Ngay15 to set
+     */
+    public void setNgay15(String Ngay15) {
+        this.Ngay15 = Ngay15;
+    }
+
+    /**
+     * @return the Ngay16
+     */
+    public String getNgay16() {
+        return Ngay16;
+    }
+
+    /**
+     * @param Ngay16 the Ngay16 to set
+     */
+    public void setNgay16(String Ngay16) {
+        this.Ngay16 = Ngay16;
+    }
+
+    /**
+     * @return the Ngay17
+     */
+    public String getNgay17() {
+        return Ngay17;
+    }
+
+    /**
+     * @param Ngay17 the Ngay17 to set
+     */
+    public void setNgay17(String Ngay17) {
+        this.Ngay17 = Ngay17;
+    }
+
+    /**
+     * @return the Ngay18
+     */
+    public String getNgay18() {
+        return Ngay18;
+    }
+
+    /**
+     * @param Ngay18 the Ngay18 to set
+     */
+    public void setNgay18(String Ngay18) {
+        this.Ngay18 = Ngay18;
+    }
+
+    /**
+     * @return the Ngay19
+     */
+    public String getNgay19() {
+        return Ngay19;
+    }
+
+    /**
+     * @param Ngay19 the Ngay19 to set
+     */
+    public void setNgay19(String Ngay19) {
+        this.Ngay19 = Ngay19;
+    }
+
+    /**
+     * @return the Ngay20
+     */
+    public String getNgay20() {
+        return Ngay20;
+    }
+
+    /**
+     * @param Ngay20 the Ngay20 to set
+     */
+    public void setNgay20(String Ngay20) {
+        this.Ngay20 = Ngay20;
+    }
+
+    /**
+     * @return the Ngay21
+     */
+    public String getNgay21() {
+        return Ngay21;
+    }
+
+    /**
+     * @param Ngay21 the Ngay21 to set
+     */
+    public void setNgay21(String Ngay21) {
+        this.Ngay21 = Ngay21;
+    }
+
+    /**
+     * @return the Ngay22
+     */
+    public String getNgay22() {
+        return Ngay22;
+    }
+
+    /**
+     * @param Ngay22 the Ngay22 to set
+     */
+    public void setNgay22(String Ngay22) {
+        this.Ngay22 = Ngay22;
+    }
+
+    /**
+     * @return the Ngay23
+     */
+    public String getNgay23() {
+        return Ngay23;
+    }
+
+    /**
+     * @param Ngay23 the Ngay23 to set
+     */
+    public void setNgay23(String Ngay23) {
+        this.Ngay23 = Ngay23;
+    }
+
+    /**
+     * @return the Ngay24
+     */
+    public String getNgay24() {
+        return Ngay24;
+    }
+
+    /**
+     * @param Ngay24 the Ngay24 to set
+     */
+    public void setNgay24(String Ngay24) {
+        this.Ngay24 = Ngay24;
+    }
+
+    /**
+     * @return the Ngay25
+     */
+    public String getNgay25() {
+        return Ngay25;
+    }
+
+    /**
+     * @param Ngay25 the Ngay25 to set
+     */
+    public void setNgay25(String Ngay25) {
+        this.Ngay25 = Ngay25;
+    }
+
+    /**
+     * @return the Ngay26
+     */
+    public String getNgay26() {
+        return Ngay26;
+    }
+
+    /**
+     * @param Ngay26 the Ngay26 to set
+     */
+    public void setNgay26(String Ngay26) {
+        this.Ngay26 = Ngay26;
+    }
+
+    /**
+     * @return the Ngay27
+     */
+    public String getNgay27() {
+        return Ngay27;
+    }
+
+    /**
+     * @param Ngay27 the Ngay27 to set
+     */
+    public void setNgay27(String Ngay27) {
+        this.Ngay27 = Ngay27;
+    }
+
+    /**
+     * @return the Ngay28
+     */
+    public String getNgay28() {
+        return Ngay28;
+    }
+
+    /**
+     * @param Ngay28 the Ngay28 to set
+     */
+    public void setNgay28(String Ngay28) {
+        this.Ngay28 = Ngay28;
+    }
+
+    /**
+     * @return the Ngay29
+     */
+    public String getNgay29() {
+        return Ngay29;
+    }
+
+    /**
+     * @param Ngay29 the Ngay29 to set
+     */
+    public void setNgay29(String Ngay29) {
+        this.Ngay29 = Ngay29;
+    }
+
+    /**
+     * @return the Ngay30
+     */
+    public String getNgay30() {
+        return Ngay30;
+    }
+
+    /**
+     * @param Ngay30 the Ngay30 to set
+     */
+    public void setNgay30(String Ngay30) {
+        this.Ngay30 = Ngay30;
+    }
+
+    /**
+     * @return the Ngay31
+     */
+    public String getNgay31() {
+        return Ngay31;
+    }
+
+    /**
+     * @param Ngay31 the Ngay31 to set
+     */
+    public void setNgay31(String Ngay31) {
+        this.Ngay31 = Ngay31;
+    }
+}
